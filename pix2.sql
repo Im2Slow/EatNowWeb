0c8908092b5a9a2dce1572c943a34f06f8370fbe
D3rpyCat <juliette.desormonts@gmail.com>
2017-04-02 12:29:16 +0200

    MAJ BDD

--- pix2.sql
+++ pix2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1:3388
--- Généré le :  Mer 22 Mars 2017 à 17:13
+-- Généré le :  Dim 02 Avril 2017 à 12:27
 -- Version du serveur :  5.6.19
 -- Version de PHP :  5.4.31
 
@@ -28,9 +28,48 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `dishes` (
 `Number` int(11) NOT NULL,
+  `ID Restaurant` int(11) NOT NULL,
   `Name` text NOT NULL,
-  `Cost` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `Type` text NOT NULL,
+  `Cost` double NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
+
+--
+-- Contenu de la table `dishes`
+--
+
+INSERT INTO `dishes` (`Number`, `ID Restaurant`, `Name`, `Type`, `Cost`) VALUES
+(1, 1, 'Gourde à boire', 'Fruit', 1.9),
+(2, 1, 'Salade de fruits', 'Fruit', 0),
+(3, 1, 'Cristaline 50cL', 'Boisson', 0),
+(4, 1, 'Liégeois au chocolat', 'Dessert', 1.9),
+(5, 1, 'PastaBOX - Fusilli à la Bolognaise', 'Plat chaud', 0),
+(6, 1, 'CremioBOX - Poulet à la crème avec Emmental', 'Plat chaud', 0),
+(7, 1, 'Muffin nature', 'Dessert', 1.8),
+(8, 1, 'CremioBOX - Jambon à la crème avec Emmental', 'Plat chaud', 0),
+(9, 1, 'PastaBOX - Fusilli à la Carbonara', 'Plat chaud', 0),
+(10, 1, 'PastaBOX - Tortellini Ricotta Epinards\r\nSauce au parmesan', 'Plat chaud', 0),
+(11, 1, 'Fusilli aux Fromages Italiens', 'Plat chaud', 0),
+(12, 1, 'Minute Maid Pomme', 'Boisson', 0),
+(13, 1, 'Fromage blanc au fruits rouges', 'Dessert', 0),
+(14, 1, 'Lipton Ice Tea Pêche', 'Boisson', 0),
+(15, 1, 'Compote', 'Fruit', 1.9),
+(16, 1, 'Cookie', 'Dessert', 1.5),
+(17, 1, 'Donut Nature', 'Dessert', 1.3),
+(18, 1, 'Cristaline Gazéifiée 0.5L', 'Boisson', 0),
+(19, 1, 'Coca-Cola', 'Boisson', 0),
+(20, 1, 'Coca-Cola Light', 'Boisson', 0),
+(21, 1, 'Cristaline 1.5cL', 'Boisson', 0),
+(22, 1, 'Banane', 'Fruit', 0),
+(23, 1, 'Minute Maid Tropical', 'Boisson', 0),
+(24, 1, 'Donut Chocolat', 'Dessert', 0),
+(25, 1, 'Beignet aux pommes', 'Dessert', 1.5),
+(26, 1, 'Brownie', '', 1.8),
+(27, 1, 'Minute Maid Orange', '', 0),
+(28, 1, 'Orangina', '', 0),
+(29, 1, 'Fanta Orange', '', 0),
+(30, 1, 'Minute Maid Rouge Sensation', '', 0),
+(31, 1, 'Coca-Cola Zero', '', 0);
 
 -- --------------------------------------------------------
 
@@ -46,7 +85,14 @@ CREATE TABLE IF NOT EXISTS `login` (
   `valide` enum('0','1','2') COLLATE latin1_general_ci NOT NULL,
   `statut` enum('0','1') COLLATE latin1_general_ci NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `login`
+--
+
+INSERT INTO `login` (`id`, `pseudo`, `pass`, `email`, `valide`, `statut`, `date`) VALUES
+(2, 'DerpyCat', '060298', 'juliette.desormonts@gmail.com', '1', '1', '2017-03-31');
 
 -- --------------------------------------------------------
 
@@ -55,11 +101,22 @@ CREATE TABLE IF NOT EXISTS `login` (
 --
 
 CREATE TABLE IF NOT EXISTS `menu` (
-`number` int(11) NOT NULL,
-  `number restaurant` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+`Number` int(11) NOT NULL,
+  `ID Restaurant` int(11) NOT NULL,
+  `Name` text NOT NULL,
+  `Description` text NOT NULL,
+  `Cost` double NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `menu`
+--
+
+INSERT INTO `menu` (`Number`, `ID Restaurant`, `Name`, `Description`, `Cost`) VALUES
+(1, 1, 'Formule Maison 1', 'Sandwich Maison\r\nDessert', 4),
+(2, 1, 'Formule Maison 2', 'Salade Maison\r\nDessert', 4.5),
+(3, 1, 'Formule Toastée', 'Panini\r\nDessert', 5),
+(4, 1, 'Formule Libertée', 'Wrap ou Sandwich Club\r\nDessert', 5.5);
 
 -- --------------------------------------------------------
 
@@ -68,11 +125,11 @@ CREATE TABLE IF NOT EXISTS `menu` (
 --
 
 CREATE TABLE IF NOT EXISTS `orders` (
-`number` int(11) NOT NULL,
-  `number user` int(11) NOT NULL,
-  `number restaurant` int(11) NOT NULL,
-  `number menu` int(11) NOT NULL,
-  `payed` tinyint(1) NOT NULL
+`Number` int(11) NOT NULL,
+  `ID Login` int(11) NOT NULL,
+  `ID Restaurant` int(11) NOT NULL,
+  `ID Menu` int(11) NOT NULL,
+  `Payed` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -85,8 +142,19 @@ CREATE TABLE IF NOT EXISTS `restaurants` (
 `Number` int(11) NOT NULL,
   `Name` tinytext NOT NULL,
   `Location` text NOT NULL,
-  `Open` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `Open` tinyint(1) NOT NULL,
+  `Type` text NOT NULL,
+  `Description` text NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `restaurants`
+--
+
+INSERT INTO `restaurants` (`Number`, `Name`, `Location`, `Open`, `Type`, `Description`) VALUES
+(1, 'Cafeteria', 'Rue Haute, 12 Avenue Leonard De Vinci, 92400 Courbevoie', 1, 'Cafe & Snacks', ''),
+(2, 'Grill', 'Rue Basse, 12 Avenue Leonard De Vinci, 92400 Courbevoie', 1, 'Restauration classique', ''),
+(3, 'Lounge', 'Rue Haute, 12 Avenue Leonard De Vinci, 92400 Courbevoie', 1, 'Cafe & Snacks', 'Ouvert de 10h à 16h');
 
 --
 -- Index pour les tables exportées
@@ -96,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `restaurants` (
 -- Index pour la table `dishes`
 --
 ALTER TABLE `dishes`
- ADD PRIMARY KEY (`Number`);
+ ADD PRIMARY KEY (`Number`), ADD KEY `ID Restaurant` (`ID Restaurant`);
 
 --
 -- Index pour la table `login`
@@ -108,13 +176,13 @@ ALTER TABLE `login`
 -- Index pour la table `menu`
 --
 ALTER TABLE `menu`
- ADD PRIMARY KEY (`number`);
+ ADD PRIMARY KEY (`Number`), ADD KEY `ID Restaurant` (`ID Restaurant`);
 
 --
 -- Index pour la table `orders`
 --
 ALTER TABLE `orders`
- ADD PRIMARY KEY (`number`), ADD KEY `number user` (`number user`), ADD KEY `number restaurant` (`number restaurant`), ADD KEY `number menu` (`number menu`);
+ ADD PRIMARY KEY (`Number`), ADD KEY `number user` (`ID Login`), ADD KEY `number restaurant` (`ID Restaurant`), ADD KEY `number menu` (`ID Menu`);
 
 --
 -- Index pour la table `restaurants`
@@ -130,27 +198,43 @@ ALTER TABLE `restaurants`
 -- AUTO_INCREMENT pour la table `dishes`
 --
 ALTER TABLE `dishes`
-MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT pour la table `login`
 --
 ALTER TABLE `login`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `menu`
 --
 ALTER TABLE `menu`
-MODIFY `number` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-MODIFY `number` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `restaurants`
 --
 ALTER TABLE `restaurants`
-MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `dishes`
+--
+ALTER TABLE `dishes`
+ADD CONSTRAINT `dishes_ibfk_1` FOREIGN KEY (`ID Restaurant`) REFERENCES `restaurants` (`Number`);
+
+--
+-- Contraintes pour la table `menu`
+--
+ALTER TABLE `menu`
+ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`ID Restaurant`) REFERENCES `restaurants` (`Number`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
